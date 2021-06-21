37aeb182afe2b4b54d572bc81fb53d47bd779604
Cargalher <cargalher@gmail.com>
2021-06-21 17:33:59 +0200

    variables created for parragraphs and for lenght

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 var_dump($_GET);
 $badWord = $_GET['***'];
 $quote = "Memories, even your most precious ones, fade surprisingly quickly. But I don’t go along with that. The memories I value most, I don’t ever see them fading.";
-
+$quote_len = strlen($quote);
+$quote_clean = str_replace('surprisingly', '***', $quote);
+$quote_clean_len = strlen($quote_clean);
 
 ?>
 <!DOCTYPE html>
@@ -15,10 +17,10 @@ $quote = "Memories, even your most precious ones, fade surprisingly quickly. But
 </head>
 <body>
     <p>
-        <?php echo $quote.strlen($quote); ?>
+        <?php echo $quote.$quote_len; ?>
     </p> 
     <p>
-        <?php echo str_replace('surprisingly', '***', $quote); ?> 
+        <?php echo $quote_clean.$quote_clean_len; ?> 
 
     </p>
 
